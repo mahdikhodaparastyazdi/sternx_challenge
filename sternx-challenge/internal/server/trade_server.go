c2261a157383f324bbfed60924b12fe6327a328d
Mahdi Khodaparast Yazdi <107419479+mahdikhodaparastyazdi@users.noreply.github.com>
2023-09-03 00:05:05 +0330

    Update trade_server.go

--- sternx-challenge/internal/server/trade_server.go
+++ sternx-challenge/internal/server/trade_server.go
@@ -21,7 +21,12 @@ func NewServer(tradeHandler handler.TradeHandler) Server {
 
 func (s *server) Run() error {
 	app := fiber.New()
-
+	app.Use(recover.New())
+	app.Use(requestid.New())
+	app.Use(logger.New(logger.Config{
+		Format:     "{\"level\":\"info\",\"task\":\"stern-x\",\"error\":\"${status}\",\"time\":\"${time}\",\"message\":\"${locals:requestid} ${latency} ${method} ${path}\"}\n",
+		TimeFormat: "2006-01-02T15:04:05-0700",
+	}))
 	app.Get("/latest-trades", s.tradeHandler.HandleLatestTrades)
 
 	err := app.Listen(":8088")
